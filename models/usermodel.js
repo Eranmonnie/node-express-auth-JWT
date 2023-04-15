94b4efa977733f1db4923ec08cb7088a793e34c3
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-15 13:49:30 +0100

    validating signup data

--- models/usermodel.js
+++ models/usermodel.js
@@ -6,33 +6,41 @@ const user_schema = new schema({
 
       "firstname":{
             type : String, 
-            required : true
+            required : [true, 'Please enter First name']
     },
 
       "last name":{
         type : String, 
-        required : true
+        required : [true, 'Please enter Last name'],
     },
 
       "username":{
         type : String, 
-        required : true,
-        unique : true,
+        required : [true, 'Please enter Username name'],
+        unique : [true, 'User with this username already exists'],
     },
 
       "email":{
         type : String, 
-        required : true,
-        unique : true,
-        lowercase : true
+        required : [true, 'Please enter email address'],
+        unique : [true, 'User with this email already exists'],
+        lowercase : true,
+        validate : [(val)=>{ 
+        //    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val))
+        // {
+        //   return (true)
+        // }
+        // we can use regex loke so to validate email but well use a third party package 
+        
+        }
+        , 'please enter a valid email']
     },
     
 
       'password': {
         type : String, 
-        required : true,
-        unique : true,
-        minLength : 8,
+        required : [true, 'Please enter password'],
+        minLength : [8, 'password must be of length 8 or more'],
     },
 
 })
